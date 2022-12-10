d5eafd491bca4ddae9ea53c8d2d27edc75ef7eb1
Nicolo` <nicolo.frison97@gmail.com>
2022-12-10 09:40:20 -0500

    Added dotenv package and created simple telegram bot

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const { Telegraf } = require('telegraf');
+require('dotenv').config();
+
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+
+//method for invoking start command
+ 
+bot.command('start', ctx => {
+  console.log(ctx.from)
+  bot.telegram.sendMessage(ctx.chat.id, 'hello there! Welcome to my new telegram bot.', {
+  })
+})
+
+bot.launch();
