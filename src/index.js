c1a5441053f5945a25c1c504fda398b6e8eacc76
Nicolo` <nicolo.frison97@gmail.com>
2022-12-10 13:15:25 -0500

    Set project type to module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const { Telegraf } = require('telegraf');
-require('dotenv').config();
+import { Telegraf } from 'telegraf';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
